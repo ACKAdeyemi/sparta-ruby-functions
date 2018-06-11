11acc21dbf4ce357ee9ef51a0967a014f07fc5b3
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2018-06-11 15:38:01 +0100

    Added two functions

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ruby Functions Lesson
+
+An introduction into function within Ruby and how to use them
+
+### Learnt: 
---
+++ app.rb
@@ -0,0 +1,14 @@
+# FUNCTIONS
+
+def say_hello
+  puts "Hello"
+end
+
+say_hello # function invoked
+
+
+def add_two_numbers num1, num2
+  puts num1 + num2
+end
+
+add_two_numbers 2, 4
